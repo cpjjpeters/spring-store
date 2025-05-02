fe33394c1847703dd496cc900bffb3a51132621b
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2025-05-02 17:55:41 +0200

    using mapstruct

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -2,9 +2,11 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,14 @@ import java.util.List;
 import static org.hibernate.Hibernate.map;
 
 /* carlpeters created on 02/05/2025 inside the package - com.codewithmosh.store.controllers */
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
 //    public UserController(UserRepository userRepository) {
 //        this.userRepository = userRepository;
@@ -32,7 +36,7 @@ public class UserController {
     public List<UserDto> getAllUsers() {
         return userRepository.findAll()
                 .stream()
-                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .map(userMapper ::userToUserDto)
                 .toList();
     }
 
@@ -48,10 +52,10 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
+        log.debug("User: {}", user);
         if (user == null) {
             return  ResponseEntity.notFound().build();
         }
-        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
-        return  ResponseEntity.ok(userDto);
+        return  ResponseEntity.ok(userMapper.userToUserDto(user));
     }
 }
--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -3,9 +3,15 @@ package com.codewithmosh.store.dtos;
 /* carlpeters created on 02/05/2025 inside the package - com.codewithmosh.store.dtos */
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+ @NoArgsConstructor
+
 public class UserDto {
     private Long id;
     private String name;
