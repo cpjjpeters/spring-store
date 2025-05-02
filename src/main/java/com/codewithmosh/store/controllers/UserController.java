a15c2faa3dd510155cc3d965c4004c6582c927b5
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2025-05-02 16:41:46 +0200

    UserController multiple Get

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import java.util.List;
 /* carlpeters created on 02/05/2025 inside the package - com.codewithmosh.store.controllers */
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -20,7 +23,7 @@ public class UserController {
 //        this.userRepository = userRepository;
 //    }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public Iterable<User> getAllUsers() {
         // This is a mock implementation. In a real application, you would fetch users from the database.
 //        return List.of(
@@ -30,5 +33,18 @@ public class UserController {
 //        );
 
         return userRepository.findAll();
+    }
+
+    @GetMapping("/count")
+    public Long getUserCount() {
+        return userRepository.count();
+    }
+    @GetMapping("/first")
+    public User getFirstUser() {
+        return userRepository.findAll().iterator().next();
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
